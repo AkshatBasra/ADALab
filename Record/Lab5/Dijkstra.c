8e827d6ecc8bbb5a54ecdcd07e0ca91dd9067c37
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2025-05-20 21:21:26 +0530

    Update Dijkstra.c

--- Record/Lab5/Dijkstra.c
+++ Record/Lab5/Dijkstra.c
@@ -22,10 +22,11 @@ void dijkstra(int n, int graph[n][n], int start)
                 u = v;
             }
         }
+        if(u == -1) break; // No reachable vertex left
         vis[u] = 1;
         for(int v = 0; v < n; v++)
         {
-            if(!vis[v] && graph[u][v] < d[v])
+            if(!vis[v] && d[u] + graph[u][v] < d[v] && graph[u][v] != INT_MAX)
             {
                 d[v] = d[u] + graph[u][v];
             }
@@ -34,30 +35,33 @@ void dijkstra(int n, int graph[n][n], int start)
     printf("Vertex\tDistance\n");
     for(int i = 0; i < n; i++)
     {
-        printf("%d\t%d\n", i, d[i]);
+        if(d[i] == INT_MAX)
+            printf("%d\tINF\n", i);
+        else
+            printf("%d\t%d\n", i, d[i]);
     }
 }
 
 int main()
 {
     int n = 5, start = 0;
-    int graph[n][n];
-    /*int graph[5][5] =
+    // int graph[n][n];
+    int graph[5][5] =
                      {
                          {0, 4, 3, 0, 0},
                          {2, 0, 6, 0, 3},
                          {0, 10, 0, 4, 0},
                          {0, 0, 3, 0, 1},
                          {0, 0, 2, 5, 0}
-                     };*/
+                     };
 
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            scanf("%d", &graph[i][j]);
-        }
-    }
+    // for(int i = 0; i < n; i++)
+    // {
+    //     for(int j = 0; j < n; j++)
+    //     {
+    //         scanf("%d", &graph[i][j]);
+    //     }
+    // }
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
@@ -68,10 +72,10 @@ int main()
             }
         }
     }
-    clock_t start1, end;
-    start1 = clock();
+    // clock_t start1, end;
+    // start1 = clock();
     dijkstra(n, graph, start);
-    end = clock();
-    printf("Clock: %lf", (end - start1) / CLK_TCK);
+    // end = clock();
+    // printf("Clock: %lf", (end - start1) / CLK_TCK);
     return 0;
 }
